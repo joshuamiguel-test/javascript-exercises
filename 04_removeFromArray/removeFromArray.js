f3c6f77089319263226db8813ec52049d7ceaf0a
Joshua Abordo <joshua_abordo@medilink.ph>
2024-11-01 18:47:30 +0800

    exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function (numberGroup, ...toRemove) {
+  return numberGroup.filter((number) => !toRemove.includes(number));
+
+  //   used filter for the numberGroup array then use .includes to check if the number(variable you passed all the array value)
+  //   is same as the toRemove array numbers. Then used ! to turn it true if the number is to remove.
+  // note: used ... collects all additional arguments passed to the function into an array named toRemove
 };
 
 // Do not edit below this line

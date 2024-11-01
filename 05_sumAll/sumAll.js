2d2513410ca6c13c6375e1a9433d9840764aeb68
Joshua Abordo <joshua_abordo@medilink.ph>
2024-11-01 19:11:13 +0800

    exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function (firstNumber, secondNumber) {
+  let sum = 0;
+  if (
+    firstNumber < 0 ||
+    secondNumber < 0 ||
+    typeof firstNumber !== "number" ||
+    typeof secondNumber !== "number" ||
+    !Number.isInteger(firstNumber) ||
+    !Number.isInteger(secondNumber)
+  ) {
+    return "ERROR";
+  }
+  const start = Math.min(firstNumber, secondNumber);
+  const end = Math.max(firstNumber, secondNumber);
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line

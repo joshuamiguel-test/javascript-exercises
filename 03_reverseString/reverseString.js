67d543e0b08b190b0b09d070b7f322356847ff73
Joshua Abordo <joshua_abordo@medilink.ph>
2024-11-01 18:12:12 +0800

    refactor code to be align on solution provided

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,9 +1,11 @@
-const reverseString = function (word) {
-  let reverseWord = "";
-  for (let i = word.length - 1; i >= 0; i--) {
-    reverseWord += word[i];
-  }
-  return reverseWord;
+const reverseString = function (string) {
+  //   let reverseWord = "";
+  //   for (let i = word.length - 1; i >= 0; i--) {
+  //     reverseWord += word[i];
+  //   }
+  //   return reverseWord;
+
+  return string.split("").reverse().join("");
 };
 
 // Do not edit below this line

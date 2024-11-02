e41b2430763f0b36e7d1afcf378321f3fe720266
Joshua Abordo <joshua_abordo@medilink.ph>
2024-11-02 16:18:11 +0800

    07 Fahrenheit and Celsius conversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (numberToConvert) {
+  celsius = ((numberToConvert - 32) * 5) / 9;
+
+  return (roundedNumber = Math.round(celsius * 10) / 10);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (numberToConvert) {
+  fahrenheit = (numberToConvert * 9) / 5 + 32;
+  return (roundedNumber = Math.round(fahrenheit * 10) / 10);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
